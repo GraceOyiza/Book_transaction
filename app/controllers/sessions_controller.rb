7fcba806b66570f50f35db3b52356cfbbc84aa0c
GraceOyiza <graceoyiza13@gmail.com>
2021-01-11 14:06:25 +0100

    Define methods for session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,24 @@
 class SessionsController < ApplicationController
+    
+        def new; end
+
+
+        def create
+          @user = User.find_by_username(params[:username])
+          if @user
+            session[:user_id] = user.id
+            flash[:success] = "Welcome #{@user.username}"
+            redirect_to books_path
+          else
+            flash.now[:danger] = 'Login in to continue.'
+            render :new
+          end
+        end
+       
+      
+        def destroy
+          session[:user_id] = nil
+          flash.now[:notice] = 'Logged out successfully'
+          render :new
+        end
 end
