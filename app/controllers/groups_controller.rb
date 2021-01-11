58ef6369fdaa5d066917a0ec4006125c063ed934
GraceOyiza <graceoyiza13@gmail.com>
2021-01-11 14:11:05 +0100

    Methods for group controller created

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,44 @@
 class GroupsController < ApplicationController
+    before_action :book_group, only: %i[show]
+    before_action :authenticate_user!
+
+  def index
+    @groups = current_user.groups.asc 
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = current_user.groups.build(group_params)
+
+    if @group.save
+      flash[:success] = "#{@group.name} Group was created successfully"
+      redirect_to groups_path
+    else
+      flash.now[:error] = @group.errors.full_messages
+      render :new
+    end
+  end
+
+  def show
+    if current_user.groups.include? @group
+      @books = nil
+      @books = @group.books.desc if @group.books.exists?
+    else
+      flash[:danger] = 'You are not allowed to view other users groups'
+      redirect_to groups_path
+    end
+  end
+
+#   private
+
+#   def book_group
+#     @group = Group.find(params[:id])
+#   end
+
+#   def group_params
+#     params.require(:group).permit(:name, :icon)
+#   end
 end
