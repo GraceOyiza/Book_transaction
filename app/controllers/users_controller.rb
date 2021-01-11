e5b20b934d29ece2040de2a0f2fbe883a34d845d
GraceOyiza <graceoyiza13@gmail.com>
2021-01-11 14:22:48 +0100

    Add methods to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,36 @@
 class UsersController < ApplicationController
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+          session[:user_id] = @user.id
+        #   logged_in?(@user)
+        #    session[creator_id] = @user.id
+          flash[:success] = 'You have signed up successfully'
+          redirect_to user_path(@user)
+        else
+            puts @user.errors.full_messages, 'ERRORS!!!!!!!!!'
+
+          flash[:danger] = 'Oops! Something went wrong!'
+          render 'new'
+        end
+      end
+
+      def show
+        redirect_to books_path
+      end
+
+
+     def destroy
+        @user = User.find( params[:id] )
+        if @user.destroy
+          flash[:info] = "Are you sure you wanna do this"
+        else
+          flash[:danger] = 'Oops! Something went wrong!'
+        end
+        redirect_to root_path
+    end
 end
