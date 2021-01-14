f18d0510bd4dbeec8a7d00af2eeb676838cd0558
GraceOyiza <graceoyiza13@gmail.com>
2021-01-14 11:58:33 +0100

    Validation added for username

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
 
     has_many :books, foreign_key: :creator_id, dependent: :destroy
     has_many :groups, foreign_key: :creator_id
+
+    validates :username, presence: true, length: { minimum: 3, maximum: 35 }
+    validates_uniqueness_of :username
 end
