e093737a138bfc7d319a931a79aa236eeae0b8d5
GraceOyiza <graceoyiza13@gmail.com>
2021-01-14 14:29:32 +0100

    Add scope to booksfor group and no group

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,4 +6,10 @@ class Book < ApplicationRecord
     validates :title, presence: true, length: { minimum: 3, maximun: 35 }
     validates :amount, presence: true
     validates_numericality_of :amount, greater_than_or_equal_to: 1
+
+    scope :desc, -> { includes(:creator).order('created_at DESC') }
+
+    scope :desc_group, -> { desc.select { |book| book.groups.exists? } }
+  
+    scope :desc_no_group, -> { desc.reject { |book| book.groups.exists? } }
 end
