cc2c8abcdba9fe3490a86f21fa4161eb11ec5113
GraceOyiza <graceoyiza13@gmail.com>
2021-01-14 11:56:06 +0100

    Add validation to book models

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,8 @@ class Book < ApplicationRecord
     belongs_to :creator, class_name: 'User'
     has_many :book_groups
     has_many :groups, through: :book_groups
+
+    validates :title, presence: true, length: { minimum: 3, maximun: 35 }
+    validates :amount, presence: true
+    validates_numericality_of :amount, greater_than_or_equal_to: 1
 end
--- db/migrate/20210111110659_create_books.rb
+++ db/migrate/20210111110659_create_books.rb
@@ -1,7 +1,11 @@
 class CreateBooks < ActiveRecord::Migration[6.0]
   def change
     create_table :books do |t|
-
+      t.string :title
+      t.integer :amount
+      t.integer :date
+      t.integer :creator_id, class_name: 'User'
+      
       t.timestamps
     end
   end
